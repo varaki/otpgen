f6a5cb521caf68ddcec24c929d0f4ed853888ce6
varaki <varady.akos@gmail.com>
2022-03-30 20:25:15 +0200

    Formatted with gofmt

--- cmd/otpgen/main.go
+++ cmd/otpgen/main.go
@@ -5,30 +5,30 @@ import (
 	"fmt"
 	"time"
 
+	clipb "github.com/atotto/clipboard"
 	"github.com/pquerna/otp/totp"
-    clipb "github.com/atotto/clipboard"
 )
 
 func main() {
 
-    var secret string
-    var clipboard bool
+	var secret string
+	var clipboard bool
 
-    flag.StringVar(&secret, "secret", "", "token secret")
-    flag.BoolVar(&clipboard, "clipboard", false, "copy generated code to clipboard")
-    flag.Parse()
+	flag.StringVar(&secret, "secret", "", "token secret")
+	flag.BoolVar(&clipboard, "clipboard", false, "copy generated code to clipboard")
+	flag.Parse()
 
-    code, err := generate(secret)
-    if err != nil {
-        panic(err)
-    }
+	code, err := generate(secret)
+	if err != nil {
+		panic(err)
+	}
 
-    if clipboard {
-        err := clipb.WriteAll(code)
-        if err != nil {
-            panic(err)
-        }
-    }
+	if clipboard {
+		err := clipb.WriteAll(code)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 func generate(secret string) (string, error) {
